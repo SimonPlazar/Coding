9a4f6bf230c271e1ad9fe812b2c371aa9bef67f8
SimonPlazar <simon.plazar@gmail.com>
2022-05-01 18:50:11 +0200

    Added vaja0902 final

--- Podatkovne Strukture/Vaja4Strukture/cmake-build-debug/cmake_install.cmake
+++ Podatkovne Strukture/Vaja4Strukture/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Coding/Vaja4Strukture
+# Install script for directory: D:/Coding/Podatkovne Strukture/Vaja4Strukture
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Coding/Vaja4Strukture/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Coding/Podatkovne Strukture/Vaja4Strukture/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
